93d0be86aeddedae292eb762177a7900d8caef39
Bran <brandonfp7@hotmail.com>
2024-08-18 13:31:43 -0700

    Version 1.02 Fixing problems and improving css

--- code/audio.js
+++ code/audio.js
@@ -1,7 +1,7 @@
 
 
-const BGaudio = new Audio("/assets/Pokémon Black & White - Trainer Battle Music (HQ).mp3")
-const lowHp = new Audio("/assets/Pokémon Black & White - Critical Health Music (HQ).mp3")
+const BGaudio = new Audio("/assets/battle1.mp3")
+const lowHp = new Audio("/assets/lowhealth.mp3")
 const body = document.getElementById("body")
 BGaudio.play()
 BGaudio.loop = true
