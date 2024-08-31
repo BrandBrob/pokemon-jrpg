39ea38e8a0b8602cf2cfb33ada33f3f7a7b73ebe
Bran <brandonfp7@hotmail.com>
2024-08-30 21:49:04 -0700

    Adding new feature to home, and improving css.

--- code/sidebar.js
+++ code/sidebar.js
@@ -3,6 +3,7 @@ const closeBtn = document.getElementById("close-menuBtn")
 const continueGame = document.getElementById("continue-game-a")
 const sidebar = document.getElementById("sidebar")
 const dataTeam = localStorage.getItem("pokemons")
+const startBtn = document.getElementById("start-btn")
 if(dataTeam){
     continueGame.href = "jrpg.html"
 }else{
@@ -14,5 +15,11 @@ openBtn.addEventListener("click",()=>{
 closeBtn.addEventListener("click",()=>{
     sidebar.classList.toggle("dis")
 })
-
+startBtn.addEventListener("click",()=>{
+    if(dataTeam){
+        window.open("jrpg.html")
+    } else{
+        window.open("selection.html")
+    }
+})
 export{}
